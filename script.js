86125127e408afb7fe08a559a1586b9ae009041f
Kamrul Hasan <mailtokamrul@protonmail.com>
2024-01-05 23:03:46 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('a');
+    links.forEach(function (link) {
+        link.setAttribute('target', '_blank');
+    });
+});

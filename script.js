458a17b83944c4d5ad14d66d3dfcaad5ebddad87
Kamrul Hasan <mailtokamrul@protonmail.com>
2024-01-16 00:56:54 +0600

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var links = document.querySelectorAll('a');
     links.forEach(function (link) {
         link.setAttribute('target', '_blank');
     });
+
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = currentYear;
 });
